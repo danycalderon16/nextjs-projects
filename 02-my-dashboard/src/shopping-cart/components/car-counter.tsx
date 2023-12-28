b31c7daf2dbf2d580ec495b6f9f51009ef03b010
Daniel Calderon <danycalvir@gmail.com>
2023-12-27 22:03:39 -0700

    counter slide

--- 02-my-dashboard/src/shopping-cart/components/car-counter.tsx
+++ 02-my-dashboard/src/shopping-cart/components/car-counter.tsx
@@ -1,28 +1,34 @@
 "use client"
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { addOne, initCounterState, substracOne } from "@/store/counter/counterSlice";
+import { useAppSelector } from "@/store/store";
 
 interface Props {
   value?:number
 }
 
 export const CarCounter = ({value = 0}:Props) => {
-  const [counter, setCounter] = useState<number>(value);
+  const count = useAppSelector(state => state.counter.count);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initCounterState(value))
+  },[dispatch, value])
+
   return (
     <>
-      <span className="text-9xl">{counter}</span>
+      <span className="text-9xl">{count}</span>
       <div className="flex">
         <button
-          onClick={() => {
-            setCounter((prev) => prev - 1);
-          }}
+        onClick={() => { dispatch(substracOne())}}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-        >
+          >
           -1
         </button>
         <button
-          onClick={() => {
-            setCounter((prev) => prev + 1);
-          }}
+          onClick={() => { dispatch(addOne()) }}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           +1
