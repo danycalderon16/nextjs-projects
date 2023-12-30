cc0987096cae14f2324b3eea92ab2d8e039ccb5e
Daniel Calderon <danycalvir@gmail.com>
2023-12-29 22:19:30 -0700

    init 3th project, create db

---
+++ 03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -0,0 +1,23 @@
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
+
+interface Segments {
+  params : {
+    id:string
+  }
+}
+
+export async function GET(request: Request, {params}:Segments) { 
+
+  const id = params.id;
+
+  const todo = await prisma.todo.findFirst({
+    where:{id}
+  })
+
+  if ( !todo ) {
+    return NextResponse.json({ message: `Todo con id ${ params.id } no exite` }, { status: 404 });
+  }
+
+  return NextResponse.json(todo);
+}
