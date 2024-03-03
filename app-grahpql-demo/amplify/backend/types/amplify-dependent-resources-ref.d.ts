d4f295ff4957748ab0540d2fd913f0cce293e42e
Daniel Calderon <danycalvir@gmail.com>
2024-03-02 22:26:15 -0700

    project graphql

---
+++ app-grahpql-demo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "pruebagraph": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
