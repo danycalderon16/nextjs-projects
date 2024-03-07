713a061f510a7daa3f924fd2b4eaa98633066a3d
Daniel Calderon <danycalvir@gmail.com>
2024-03-07 16:22:09 -0700

    list videos from yt

---
+++ app-grahpql-demo/app/test-api/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+import Image from "next/image";
+import React, { useState } from "react";
+
+const  API_KEY  = process.env.YOUTUBE_API_KEY
+export default function Page() {
+  const [search, setSearch] = useState<string>("");
+  const [videos, setVideos] = useState([]);
+
+  const handleSearch = () => {
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${"AIzaSyC6CrOgFhWZ1wOKc2o6e3dYvU-yO_BirhQ"}&type=video&part=snippet&maxResults=${20}&q=${search}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setVideos(data.items);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching videos:', error);
+      });
+  }
+
+  return (
+    <main className="flex flex-col min-h-screen items-center gap-2 p-5">
+      
+        <div className="flex gap-1 w-full">
+          <input 
+          className="p-2 w-full  rounded-md"
+          type="text" 
+          name="search" 
+          onChange={(e)=> setSearch(e.target.value)}
+          value={search} />
+          <button 
+          className="p-2 rounded-md bg-blue-500 text-white" 
+          onClick={()=>handleSearch()}>Search</button>
+        </div>
+        {
+          videos.length > 0 && (
+            <div className="grid grid-cols-4 gap-3">
+{
+            videos.map((video:any, index:number) => (
+            <div className="flex flex-col">
+              <Image src={video.snippet.thumbnails.high.url} alt="thumbnail" width={220} height={80}/>
+              <p className="text-base">{video.snippet.title}</p>
+              <p className="text-sm">{video.snippet.channelTitle}</p>
+            </div>
+          ))}
+            </div>)
+        }
+    </main>
+  );
+}
---
+++ app-grahpql-demo/app/watch/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import CreateItem from "@/components/create-item";
+import { GET_USUARIO_QUERY } from "@/graphql/queries";
+import { useItems } from "@/hooks/useItems";
+import { generateClient } from "aws-amplify/api";
+const client = generateClient();
+
+export default function Name() {
+  const { items, loading } = useItems();
+  const handleOneUser = async (id:string) => {
+
+    const result:any = await client.graphql({
+      query: GET_USUARIO_QUERY,
+      variables: {
+        id
+      }
+    });
+    
+    const keys = Object.entries(result.data.listActividades);
+    
+    alert(keys)
+  }
+
+  
+
+  return (
+    <main className="flex min-h-screen items-center justify-evenly gap-2 p-24">
+      <div className="grid grid-cols-3 gap-2">
+      {
+        loading ?
+        <p>Loading...</p>:
+        items.map((user:any)=>(
+          <div 
+          onClick={()=>handleOneUser(user.id)}
+          className="border border-gray-800 p-2 rounded-md cursor-pointer hover:shadow-md hover:scale-105"          
+          key={user.id}>
+          
+          <p>{user.url_video}</p>
+          </div>
+        ))
+      }
+      </div>
+      <CreateItem />
+    </main>
+  );
+}
