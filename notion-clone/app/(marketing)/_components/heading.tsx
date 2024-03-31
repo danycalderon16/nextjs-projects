056b127c6dd025d29b9a300c6be89cd261c910f7
Daniel Calderon <danycalvir@gmail.com>
2024-03-30 21:52:59 -0700

    auth

--- notion-clone/app/(marketing)/_components/heading.tsx
+++ notion-clone/app/(marketing)/_components/heading.tsx
@@ -1,15 +1,45 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
-import React from 'react'
+"use client";
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  console.log(isLoading);
+  
   return (
-    <div className='max-w-3xl space-y-4 sm:text-5xl md:text-6xl font-bold'>
-      <h1>Your Ideas, Documents, & Plans. Unified. Welcome to <span className='underline'>Dotion</span></h1>
-      <h3 className='text-base sm:text-xl md:text-2xl font-medium'>Dtion is the connected workspace where <br/>better, faster work happens</h3>
-      <Button>Enter Jotion <ArrowRight className='h-4 w-4 ml-2'/></Button>
-
+    <div className="max-w-3xl space-y-4 sm:text-5xl md:text-6xl font-bold">
+      <h1>
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Dotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Dtion is the connected workspace where <br />
+        better, faster work happens
+      </h3>
+      {isLoading && (
+        <div className="flex justify-center items-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button size="sm">
+            Get Dotion Free <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
-  )
-}
+  );
+};
---
+++ notion-clone/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://creative-glowworm-24.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
