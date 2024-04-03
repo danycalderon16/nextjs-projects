3405d9aec8d5968302d929038eaff25d890a5f8e
Daniel Calderon <danycalvir@gmail.com>
2024-04-02 22:47:29 -0700

    archive functionability

--- notion-clone/app/(main)/_components/item.tsx
+++ notion-clone/app/(main)/_components/item.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, MoreHorizontalIcon, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -36,12 +38,27 @@ export const Item = ({
 }: Props) => {
 
   const create = useMutation(api.docuements.create);
+  const archive = useMutation(api.docuements.archive);  
   const router = useRouter();
+  const {user} = useUser()
 
   const handleExpand = (event:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
     onExpand?.();
   }
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if(!id) return;
+    const promise = archive({id})
+    
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
+    })
+  }
+
 
   const onCreate = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -108,6 +125,32 @@ export const Item = ({
       )}
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              onClick={(e)=> e.stopPropagation()}
+              asChild
+            >
+              <div
+                role="button"
+                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+              >
+                <MoreHorizontal className="h-4 w-4 to-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount
+            >
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="h-4 w-4 mr-2"/>
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator/>
+              <div className="text-xs text-muted-foreground">Last edited by: {user?.fullName}</div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div 
           role="button"
           onClick={onCreate}
--- notion-clone/convex/docuements.ts
+++ notion-clone/convex/docuements.ts
@@ -1,9 +1,46 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { Id } from "./_generated/dataModel";
+
+export const archive = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not authenticated");
+
+    const userId = identity.subject;
+
+    const existingDocuemnt = await ctx.db.get(args.id);
+
+    if (!existingDocuemnt) throw new Error("Document not found");
+
+    if (existingDocuemnt.userId !== userId) throw new Error("Unauthorized");
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q)=> (
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        ))
+        .collect();
+      for (const child of children) {
+        await ctx.db.patch(child._id, {isArchived: true});
+
+        await recursiveArchive(child._id);
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, {isArchived: true});
+    
+    return document;
+  },
+});
 
 export const getSidebar = query({
-  args:{
-    parentDocument: v.optional(v.id("documents"))
+  args: {
+    parentDocument: v.optional(v.id("documents")),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -11,16 +48,15 @@ export const getSidebar = query({
 
     const userId = identity.subject;
     const documents = await ctx.db
-    .query("documents")
-    .withIndex("by_user_parent", (q) => 
-      q.eq("userId", userId).eq("parentDocument", args.parentDocument)
-    )
-    .filter((q)=>q.eq(q.field("isArchived"),false))
-    .order("desc")
-    .collect();
-    return documents
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+    return documents;
   },
-
 });
 
 export const create = mutation({
