44566c3c310401b6951b74a58ccbbe0c717b4e29
Daniel Calderon <danycalvir@gmail.com>
2024-04-04 23:09:17 -0700

    init command

--- notion-clone/components/search-command.tsx
+++ notion-clone/components/search-command.tsx
@@ -5,13 +5,30 @@ import { File } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import { CommandDialog } from '@/components/ui/command'
+import { api } from '@/convex/_generated/api'
+import { useSearch } from '@/hooks/use-search'
 
 export const SearchCommand = () => {
 
   const {user } = useUser();
   const router = useRouter(); 
-;  
+  const docuemnts = useQuery(api.docuements.getSearch)
+  const [isMounted, setIsMounted] = useState(false)
+
+  const toggle = useSearch((store)=> store.toggle)
+  const isOpen = useSearch((store)=> store.isOpen)
+  const onClose = useSearch((store)=> store.onClose)
+
+  useEffect(()=>{
+    setIsMounted(true);
+
+  },[])
+  
+  if(!isMounted){
+    return null;
+  }
+
   return (
-    <CommandDialog>search-command</CommandDialog>
+    <CommandDialog open={isOpen} onOpenChange={onClose}>search-command</CommandDialog>
   )
 }
