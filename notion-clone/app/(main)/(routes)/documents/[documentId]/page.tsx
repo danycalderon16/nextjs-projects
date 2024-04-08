f320b129d4a10964280b1327cc1af60c8a7ff6c6
Daniel Calderon <danycalvir@gmail.com>
2024-04-07 19:25:40 -0700

    toolbar

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,32 @@
+"use client"
+import { Toolbar } from '@/components/toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useQuery } from 'convex/react'
 import React from 'react'
 
-const DocuemntIdPage = () => {
+interface Props {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocuemntIdPage = ({params}:Props) => {
+  const document = useQuery(api.docuements.getById,{
+    documentId: params.documentId
+  });
+
+  if (document ===  undefined) return <div>Loading...</div>
+
+  if( document === null) return <div>Document not found</div>
+
   return (
-    <div>DocuemntPage</div>
+    <div className="pb-40">
+      <div className='h-[35vh]'></div>
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document}/>
+      </div>
+    </div>
   )
 }
 
---
+++ notion-clone/components/toolbar.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Doc } from '@/convex/_generated/dataModel'
+import React from 'react'
+import { IconPicker } from './icon-picker'
+import { Button } from './ui/button'
+import { X } from 'lucide-react'
+
+interface Props {
+  initialData: Doc<"documents">
+  preview?: boolean
+}
+
+export const Toolbar = ({
+  initialData,
+  preview
+}:Props) => {
+  return (
+    <div className='pl-[54px] group relative'>
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={()=> {}}>
+            <p className='text-6xl hover:opacity-75 transition' >{initialData.icon}</p>
+          </IconPicker>
+          <Button
+            onClick={()=> {}}
+            className='rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs'>
+            <X className='h-4 w-4'/>
+          </Button>
+        </div>
+      )}
+    </div>
+  )
+}
