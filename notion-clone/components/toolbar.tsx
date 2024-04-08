f0503e7c21f19ec2ba152fdb3f0c18ff0634aadb
Daniel Calderon <danycalvir@gmail.com>
2024-04-07 19:31:00 -0700

    emoji picker

--- notion-clone/components/toolbar.tsx
+++ notion-clone/components/toolbar.tsx
@@ -4,7 +4,7 @@ import { Doc } from '@/convex/_generated/dataModel'
 import React from 'react'
 import { IconPicker } from './icon-picker'
 import { Button } from './ui/button'
-import { X } from 'lucide-react'
+import { Smile, X } from 'lucide-react'
 
 interface Props {
   initialData: Doc<"documents">
@@ -24,11 +24,33 @@ export const Toolbar = ({
           </IconPicker>
           <Button
             onClick={()=> {}}
-            className='rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs'>
+            className='rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs'
+            variant="outline"
+            size="icon">
+            
             <X className='h-4 w-4'/>
           </Button>
         </div>
       )}
+      {!!initialData.icon && preview && (
+        <p className='text-6xl pt-6'>
+          {initialData.icon }
+        </p>
+      )}
+      <div className='opacity-100 group-hover:opacity-100 flex items-center gap-x-1 py-4'>
+        {!initialData.icon && !preview && (
+          <IconPicker onChange={()=> {}}>
+           <Button
+            className='text-muted-foreground text-xs'
+            variant="outline"
+            size="sm"
+           >
+            <Smile className='h-4 w-4 mr-2'/>
+            Add icon
+           </Button>
+          </IconPicker>
+        )}
+      </div>
     </div>
   )
 }
